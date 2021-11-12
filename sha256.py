356e98ac1d42aa84997b7654563fc60026cff9a9
d1joseph <dhiv.joseph@gmail.com>
2021-11-12 18:53:00 +1100

    tidy up

--- sha256.py
+++ sha256.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
-# SHA 256 python implementation as a command line executable that encrypts input and
-# returns a 256 bit length output has a hash digest.
-# Inputs can be user data or media file types. Anything really.
+# A python implementation of the SHA256 hash algorithm as a command line executable that takes a parameter input (data) and
+# returns the 256 bit length output has a hash digest.
+# I wrote this as a learning exercise to better understand cryptopgrahic hash functions and primitives.
+
 """
 * Filename:   sha256.py
 * Author:     Dhiv Joseph
@@ -11,13 +12,18 @@
 * Description:  Implementation of the SHA-256 hashing algorithm as a cmdlet.
                 SHA-256 is one of the three algorithms in the SHA2 spec.
                 The others, SHA-384 and SHA-512, are not included.
+                
                 This code is written and intended for educational purposes only and
                 should not be implemented in anything resembling a production
-                environment.
-                Algorithm spec can be found here:
+                environment, application or encryption method.
+                
+                The NIST algorithm spec can be found here:
                 # http://csrc.nist.gov/publications/fips/fips180-2/fips180-2withchangenotice.pdf
 
+                This algorithm was tested according the NIST standard. 
+
 """
+
 __author__ = 'Dhiv Joseph'
 __licence__ = 'MIT'
 import copy
@@ -25,12 +31,14 @@ import struct
 import binascii
 from tests.keys import keyGen
 
-print(keyGen())
+# TESTS
+# print(keyGen())
 
 # hash constants (h) - 8 hash values of the first 32 bits of the fractional parts of the
 # square roots of the first 8 primes: 2, 3, 5, 7, 11, 13, 17, 19.
 h = [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0xa54ff53a,
     0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x1f83d9ab, 0x5be0cd19]
+
 # round constants (k) - 64 hash values representing the first 32 bits of the fractional parts
 # of the cube roots of the first 64 primes (2 - 311).
 k = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -43,11 +51,10 @@ k = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x9
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
 
 
-# pre-processing raw data into 512 bit message blocks for message digesting in the scheduler.
+# pre-process the raw data into 512 bit message blocks (chunks) to digest and moved into the scheduler.
 def pre_process(raw_data):
     """
     """
-
     # raw value converted to binary representation as a string
     msg = ''.join(format(i, '08b') for i in bytearray(raw_data, encoding='utf-8'))
 
