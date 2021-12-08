6b4c9db9390c6e02e850d27819950e7af2e330f5
d1joseph <dhiv.joseph@gmail.com>
2021-12-09 01:53:02 +1100

    tidy up and refactor/reorganise codebase following more research.

--- sha256.py
+++ sha256.py
@@ -20,18 +20,16 @@
 """
 __author__ = 'Dhiv Joseph'
 __licence__ = 'MIT'
-import copy
-import struct
-import binascii
 
 
 # hash constants (h) - 8 hash values of the first 32 bits of the fractional parts of the
 # square roots of the first 8 primes: 2, 3, 5, 7, 11, 13, 17, 19.
-h = [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0xa54ff53a,
+H = [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0xa54ff53a,
     0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x1f83d9ab, 0x5be0cd19]
+
 # round constants (k) - 64 hash values representing the first 32 bits of the fractional parts
 # of the cube roots of the first 64 primes (2 - 311).
-k = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+K = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
     0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
@@ -40,54 +38,90 @@ k = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x9
     0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
 
+class SHA256:
 
-# pre-processing raw data into 512 bit message blocks for message digesting in the scheduler.
-def pre_process(raw_data):
-    """
-    """
+    def __init__(self):
+        pass
 
-    # raw value converted to binary representation as a string
-    msg = ''.join(format(i, '08b') for i in bytearray(raw_data, encoding='utf-8'))
-
-    # binary repr of the converted msg length in bits 
-    msg_len_endian = bin(len(msg))
     
-    # add zeroes for padding to until message block is evenly divisible by 512.
-    # reserve 64 bits on the end to encode the len of original message. 
-    msg_block = msg + '1' + ''
+    def translation(self, message):
+        """
+        """
+        charcodes = [ord(c) for c in message]
+        
+        bytes = []
+        for char in charcodes:
+            bytes.append(bin(char)[2:].zfill(8))
+        
+        bits = []
+        for byte in bytes:
+            for bit in byte:
+                bits.append(int(bit))
 
-    while len(msg_block) % 512 != 0:
-        msg_block += '0'
+        return bits
+
+    # utility function - base conversion
+    def base_conversion(self, value):
+        """
+        """
+        value = ''.join([str(x) for x in value])
+
+        binaries = []
+        for d in range(0, len(value), 4):
+            binaries.append('0b' + value[d:d+4])
+
+        hexes = ''
+        for b in binaries:
+            hexes += hex(int(b, 2))[2:]
+        
+        return hexes
+
+    # utility function - base zero filler
+    def zero_fill(self, bits, length=8, endian='LE'):
+        """
+        """
+        l = len(bits)
+        
+        if endian == 'LE':
+            for i in range(1, length):
+                bits.append(0)
+        
+        else:
+            while l < length:
+                bits.insert(0, 0)
+                l = len(bits)
+        
+        return bits
+
+    # utility function - chunk messages into 512 bit chunks
+    def chunker(self, bits, chunk_length=8):
+        """
+        """
+        chunked = []
+        for b in range(0, len(bits), byte_length):
+            chunked.append(bits[b:b+byte_length])
+
+        return chunked
+
+    # initialize values
+    def initializer(self, values):
+        """
+        """
+        binaries = [bin(int(v, 16))[2:] for v in values]
+        words = []
+        for binary in binaries:
+            word = []
+            for b in binary:
+                word.append(int(b))
+            words.append(zero_zill(word, 32, 'BE'))
     
-    # std outputs
-    print(f'Original data: {raw_data}\nType: {type(raw_data)}\n')
-    print(f'Message converted to binary: \n{msg}\n')
-    print(f'Current message block (unpadded) with 1 bit separator: \n{msg_block}\n')
-    print(f'Current message block bit length: {len(msg_block)}')
 
-
-# main
-def sha256(data_input):
-    """
-    """
-    pass
-
-
-# WIP
-# 1. optimize pre-process routine and validate output for step 2 (meets message block properties) 
-# 2. message schedule routine
-# 3. compression function main loop
-# 4. operators routine
-# 5. structure main algo class
-# 6. write tests using NIST test specs
-# 7. testing with pytest
-# 8. Pep8 everything
-# 9. cmdlet module
-# 10. test cmdlet use cases
-# 11. setup tools and package
-# 12. final tests
-# 13. deploy
-
+    # message preparation
+    def pre_process_message(message):
+        """
+        """
+        
 
 if __name__ == "__main__":
-    pre_process('hello world')
+    sha = SHA256()
+    
